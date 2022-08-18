3ca387126763e3eb54acb2443df60c81a036936e
AntonioHincapie <marcohincapiem@gmail.com>
2022-08-18 16:28:39 -0500

    Solve linters

--- solver.rb
+++ solver.rb
@@ -8,11 +8,11 @@ class Solver
   end
 
   def fizzbuzz(number)
-    if number % 3 == 0 && number % 5 == 0
+    if (number % 3).zero? && (number % 5).zero?
       'fizzbuzz'
-    elsif number % 3 == 0
+    elsif (number % 3).zero?
       'fizz'
-    elsif number % 5 == 0
+    elsif (number % 5).zero?
       'buzz'
     else
       number
